2b2822b3155f55bb4f6da42ba9bd8eaaf0f04232
jruaux <jruaux@juliens-mbp>
2023-10-25 21:52:13 -0700

    Fixed Trino requirements for unit test resources

--- plugin/trino-redis/src/test/java/io/trino/plugin/redis/TestRediSearchConnectorSmokeTest.java
+++ plugin/trino-redis/src/test/java/io/trino/plugin/redis/TestRediSearchConnectorSmokeTest.java
@@ -27,48 +27,43 @@ import io.trino.sql.parser.ParsingException;
 import io.trino.testing.BaseConnectorSmokeTest;
 import io.trino.testing.QueryRunner;
 import io.trino.testing.TestingConnectorBehavior;
-import io.trino.testing.sql.TestTable;
 import org.testng.SkipException;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.List;
 
-import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_INSERT;
-import static io.trino.testing.TestingConnectorBehavior.SUPPORTS_UPDATE;
 import static io.trino.tpch.TpchTable.CUSTOMER;
 import static io.trino.tpch.TpchTable.NATION;
 import static io.trino.tpch.TpchTable.ORDERS;
 import static io.trino.tpch.TpchTable.REGION;
 import static java.util.Objects.requireNonNull;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class TestRediSearchConnectorSmokeTest
         extends BaseConnectorSmokeTest
 {
     private static final Logger log = Logger.get(TestRediSearchConnectorSmokeTest.class);
-
-    private RediSearchServer redisearch;
+    private static final String EMPTY_INDEX = "emptyidx";
+    private static final String JSON_INDEX = "jsontest";
+    private static final String JSON_PREFIX = "json:";
 
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
-        redisearch = new RediSearchServer();
+        RediSearchServer redisearch = closeAfterClass(new RediSearchServer());
         redisearch.getConnection().sync().flushall();
-        return RediSearchQueryRunner.createRediSearchQueryRunner(redisearch, CUSTOMER, NATION, ORDERS, REGION);
-    }
-
-    private void populateBeers()
-            throws IOException, InterruptedException
-    {
         try (StatefulRedisModulesConnection<String, String> connection = RedisModulesUtils
                 .connection(redisearch.getClient())) {
             Beers.populateIndex(connection);
+            RedisModulesCommands<String, String> sync = connection.sync();
+            CreateOptions<String, String> options = CreateOptions.<String, String>builder().prefix(EMPTY_INDEX + ":").build();
+            sync.ftCreate(EMPTY_INDEX, options, Field.tag("field1").build());
+            sync.ftCreate(JSON_INDEX, CreateOptions.<String, String>builder().on(DataType.JSON).prefix(JSON_PREFIX).build(),
+                    Field.tag("$.id").as("id").build(), Field.text("$.message").as("message").build());
+            sync.jsonSet(JSON_PREFIX + "1", "$", "{\"id\": \"1\", \"message\": \"this is a test\"}");
+            sync.jsonSet(JSON_PREFIX + "2", "$", "{\"id\": \"2\", \"message\": \"this is another test\"}");
         }
+        return RediSearchQueryRunner.createRediSearchQueryRunner(redisearch, CUSTOMER, NATION, ORDERS, REGION);
     }
 
     @Override
@@ -78,9 +73,6 @@ public class TestRediSearchConnectorSmokeTest
             case SUPPORTS_CREATE_SCHEMA:
             case SUPPORTS_CREATE_VIEW:
                 return false;
-
-            case SUPPORTS_CREATE_TABLE:
-                return true;
 
             case SUPPORTS_ARRAY:
                 return false;
@@ -99,11 +91,6 @@ public class TestRediSearchConnectorSmokeTest
 
             case SUPPORTS_NOT_NULL_CONSTRAINT:
                 return false;
-
-            case SUPPORTS_DELETE:
-            case SUPPORTS_INSERT:
-            case SUPPORTS_UPDATE:
-                return true;
 
             case SUPPORTS_RENAME_TABLE_ACROSS_SCHEMAS:
                 return false;
@@ -137,31 +124,22 @@ public class TestRediSearchConnectorSmokeTest
     public void testRediSearchFields()
             throws IOException, InterruptedException
     {
-        populateBeers();
         getQueryRunner().execute("select id, last_mod from beers");
         getQueryRunner().execute("select __key from beers");
     }
 
-    @SuppressWarnings("unchecked") @Test
+    @Test
     public void testCountEmptyIndex()
             throws IOException, InterruptedException
     {
-        String index = "emptyidx";
-        CreateOptions<String, String> options = CreateOptions.<String, String>builder().prefix(index + ":").build();
-        redisearch.getConnection().sync().ftCreate(index, options, Field.tag("field1").build());
-        assertQuery("SELECT count(*) FROM " + index, "VALUES 0");
+        assertQuery("SELECT count(*) FROM " + EMPTY_INDEX, "VALUES 0");
     }
 
-    @SuppressWarnings("unchecked") @Test
+    @Test
     public void testJsonSearch()
             throws IOException
     {
-        RedisModulesCommands<String, String> sync = redisearch.getConnection().sync();
-        sync.ftCreate("jsontest", CreateOptions.<String, String>builder().on(DataType.JSON).build(),
-                Field.tag("$.id").as("id").build(), Field.text("$.message").as("message").build());
-        sync.jsonSet("doc:1", "$", "{\"id\": \"1\", \"message\": \"this is a test\"}");
-        sync.jsonSet("doc:2", "$", "{\"id\": \"2\", \"message\": \"this is another test\"}");
-        getQueryRunner().execute("select id, message from jsontest");
+        getQueryRunner().execute("select id, message from " + JSON_INDEX);
     }
 
     @Test
@@ -174,28 +152,6 @@ public class TestRediSearchConnectorSmokeTest
     public void testShowCreateTable()
     {
         throw new SkipException("Not supported by RediSearch connector");
-    }
-
-    @SuppressWarnings("unchecked") @Test
-    public void testInsertIndex()
-            throws IOException, InterruptedException
-    {
-        String index = "insertidx";
-        String prefix = index + ":";
-        CreateOptions<String, String> options = CreateOptions.<String, String>builder().prefix(prefix).build();
-        redisearch.getConnection().sync().ftCreate(index, options, Field.tag("id").build(), Field.tag("name").build());
-        assertUpdate(String.format("INSERT INTO %s (id, name) VALUES ('abc', 'mybeer')", index), 1);
-        assertThat(query(String.format("SELECT id, name FROM %s", index)))
-                .matches("VALUES (VARCHAR 'abc', VARCHAR 'mybeer')");
-        List<String> keys = redisearch.getConnection().sync().keys(prefix + "*");
-        assertEquals(keys.size(), 1);
-        assertTrue(keys.get(0).startsWith(prefix));
-    }
-
-    @AfterClass(alwaysRun = true)
-    public final void destroy()
-    {
-        redisearch.close();
     }
 
     static RuntimeException getTrinoExceptionCause(Throwable e)
@@ -244,32 +200,5 @@ public class TestRediSearchConnectorSmokeTest
     public void testInPredicateNumeric()
     {
         assertQuery("SELECT name, regionkey FROM nation WHERE regionKey in (1, 2, 3)");
-    }
-
-    @SuppressWarnings("resource") @Test
-    public void testUpdate()
-    {
-        if (!hasBehavior(SUPPORTS_UPDATE)) {
-            // Note this change is a no-op, if actually run
-            assertQueryFails("UPDATE nation SET nationkey = nationkey + regionkey WHERE regionkey < 1",
-                    "This connector does not support updates");
-            return;
-        }
-
-        if (!hasBehavior(SUPPORTS_INSERT)) {
-            throw new AssertionError("Cannot test UPDATE without INSERT");
-        }
-
-        try (TestTable table = new TestTable(getQueryRunner()::execute, "test_update_",
-                getCreateTableDefaultDefinition())) {
-            assertUpdate("INSERT INTO " + table.getName() + " (a, b) SELECT regionkey, regionkey * 2.5 FROM region",
-                    "SELECT count(*) FROM region");
-            assertThat(query("SELECT CAST(a AS bigint), b FROM " + table.getName()))
-                    .matches(expectedValues("(0, 0.0), (1, 2.5), (2, 5.0), (3, 7.5), (4, 10.0)"));
-
-            assertUpdate("UPDATE " + table.getName() + " SET b = b + 1.2 WHERE a % 2 = 0", 3);
-            assertThat(query("SELECT CAST(a AS bigint), b FROM " + table.getName()))
-                    .matches(expectedValues("(0, 1.2), (1, 2.5), (2, 6.2), (3, 7.5), (4, 11.2)"));
-        }
     }
 }
